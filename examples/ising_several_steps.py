4cc91490bfda3618651e4ef40a89a431f8d85727
Kevin Hemery <kevin.hemery@quantinuum.com>
2024-01-03 15:07:11 +0100

    Problem with the testing

--- examples/ising_several_steps.py
+++ examples/ising_several_steps.py
@@ -22,8 +22,8 @@ z_list: list[PauliString] = []
 x_list: list[PauliString] = []
 y_list: list[PauliString] = []
 bc_modifier = 1
-nx = 2
-ny = 2
+nx = 3
+ny = 3
 n = nx*ny
 for i in range(n):
     zi = PauliString.from_pauli_sequence(paulis=[Pauli.Z], start_qubit=i)
@@ -33,8 +33,8 @@ for i in range(n):
     yi = PauliString.from_pauli_sequence(paulis=[Pauli.Y], start_qubit=i)
     y_list.append(yi)
 term_list = []
-for i in range(n):
-    term_list.append(1.0 * z_list[i])
+# for i in range(n):
+#     term_list.append(1.0 * z_list[i])
 for i in range(n):
     term_list.append(1.0 * x_list[i])
 V = -1
@@ -62,7 +62,7 @@ for site in start_sites:
 
 # Ising model
 h_ising = Hamiltonian(pauli_string_list=term_list)
-time_list=[0.05]
+time_list=[0.4]
 ed =  ED(number_of_qubits=n)
 h_ising_matrix = ed.get_hamiltonian_matrix(hamiltonian=h_ising)
 
@@ -73,68 +73,99 @@ for site in range(n-1):
 z_t_continous = []
 z_t_trotter = []
 z_t_variational = []
+z_t_variational_c = []
 energy_trotter = []
 energy_variational = []
+energy_variational_c = []
 # Get the observable for the continuous time evolution
 state_init = np.array([1.0+0.0j]+[0.0]*(2**n-1))
 energy = state_init.T@h_ising_matrix@state_init
+# print("unconstrained ")
+# print("-------")
 for time in time_list:
     variational_unitary = VU(h_ising, number_of_layer=3, time=time)
     variational_unitary.set_theta_to_trotter()
-    trotter_unitary = copy.deepcopy(variational_unitary)
+    # trotter_unitary = copy.deepcopy(variational_unitary)
     opt = Optimizer()
     res = opt.optimize_arbitrary(variational_unitary=variational_unitary,order=2,unconstrained=True)
     print("res ",res)
     c2 = variational_unitary.c2_squared(variational_unitary.theta)
     print("c2 ",c2)
     c2_trotter = variational_unitary.c2_squared(variational_unitary.get_initial_trotter_vector())
+    # breakpoint()
+# print("------")
+# print("constrained")
+for time in time_list:
+    variational_unitary_c = VUC(h_ising,number_of_layer=3,time=time)
+    variational_unitary_c.set_theta_to_trotter()
+    trotter_unitary = copy.deepcopy(variational_unitary)
+    opt = Optimizer()
+    res = opt.optimize_arbitrary(variational_unitary=variational_unitary_c,order=2,unconstrained=False)
+    # print("res ",res)
+    # c2 = variational_unitary.c2_squared(variational_unitary.theta)
+    # print("c2 ",c2)
+    # c2_trotter = variational_unitary.c2_squared(variational_unitary.get_initial_trotter_vector())
+# breakpoint()
 
 state_variational = copy.deepcopy(state_init)
+state_variational_c = copy.deepcopy(state_init)
 state_trotter= copy.deepcopy(state_init)
 state_continuous = copy.deepcopy(state_init)
 # Check that the norm is better for the variational unitary
 trotter_error = ed.get_error(trotter_unitary,hamiltonian=h_ising)
 variational_error = ed.get_error(variational_unitary=variational_unitary,hamiltonian=h_ising)
+variational_c_error = ed.get_error(variational_unitary=variational_unitary_c,hamiltonian=h_ising)
 print("trotter error", trotter_error)
 print("variational error", variational_error)
+print("variational constrained error", variational_c_error)
+breakpoint()
 
 for istep in range(10):
     state_trotter = ed.apply_variational_to_state(trotter_unitary,state_trotter)
     state_continuous = ed.apply_continuous_to_state(hamiltonian=h_ising,time=time,state = state_continuous)
     state_variational = ed.apply_variational_to_state(variational_unitary,state_variational)
+    state_variational_c = ed.apply_variational_to_state(variational_unitary_c,state_variational_c)
 
     sz_expectation_value_trotter= expectation_value(state_trotter,z_list_sparse)
     sz_expectation_value_continuous= expectation_value(state_continuous,z_list_sparse)
     sz_expectation_value_variational = expectation_value(state_variational,z_list_sparse)
+    sz_expectation_value_variational_c = expectation_value(state_variational_c,z_list_sparse)
 
     z_t_continous.append(sz_expectation_value_continuous)
     z_t_trotter.append(sz_expectation_value_trotter)
     z_t_variational.append(sz_expectation_value_variational)
+    z_t_variational_c.append(sz_expectation_value_variational_c)
 
     energy_trotter.append(state_trotter.T.conj()@h_ising_matrix@state_trotter)
     energy_variational.append(state_variational.T.conj()@h_ising_matrix@state_variational)
+    energy_variational_c.append(state_variational_c.T.conj()@h_ising_matrix@state_variational_c)
 
 z_array_continuous = np.array(z_t_continous) 
 z_array_trotter = np.array(z_t_trotter) 
 z_array_variational = np.array(z_t_variational)
+z_array_variational_c = np.array(z_t_variational_c)
 energy_trotter = np.array(energy_trotter)
 energy_variational= np.array(energy_variational)
+energy_variational_c= np.array(energy_variational_c)
 
 plt.figure()
 plt.plot(z_array_continuous[:,0],label='continuous')
 plt.plot(z_array_trotter[:,0],label='trotter')
 plt.plot(z_array_variational[:,0],label ='variational')
+# plt.plot(z_array_variational_c[:,0],label ='variational constrained')
 plt.legend(loc='best')
 plt.figure()
 plt.plot(z_array_continuous[:,int(n/2)],label='continuous')
 plt.plot(z_array_trotter[:,int(n/2)],label='trotter')
 plt.plot(z_array_variational[:,int(n/2)],label ='variational')
+# plt.plot(z_array_variational_c[:,int(n/2)],label ='variational constrained')
 plt.xlabel('time step')
 plt.ylabel(r'$\langle Z \rangle$')
 plt.legend(loc='best')
 plt.figure()
-plt.plot(np.abs(z_array_continuous[:,int(n/2)]-z_array_trotter[:,int(n/2)]),label='err trotter')
+plt.plot(np.abs(z_array_continuous[:,int(n/2)]-z_array_trotter[:,int(n/2)]),linestyle='--',marker = 'o',label='err trotter')
 plt.plot(np.abs(z_array_continuous[:,int(n/2)]-z_array_variational[:,int(n/2)]),label ='variational')
+# plt.plot(np.abs(z_array_continuous[:,int(n/2)]-z_array_variational_c[:,int(n/2)]),label ='variational constrained')
 plt.xlabel('time step')
 plt.ylabel('error')
 plt.legend(loc='best')
@@ -142,6 +173,7 @@ plt.show()
 plt.figure()
 plt.plot(np.abs(np.array(energy_trotter)-energy),label = 'trotter')
 plt.plot(np.abs(np.array(energy_variational)-energy),label = 'variational')
+# plt.plot(np.abs(np.array(energy_variational_c)-energy),label = 'variational constrained')
 plt.xlabel('time step')
 plt.ylabel('energy error')
 plt.legend(loc='best')
