07f06f43b7a401f53ee1ad85be9e29d3263b28dd
Kevin Hemery <kevin.hemery@quantinuum.com>
2024-01-23 14:55:49 +0100

    Finished first implementation of the gradient

--- src/peropq/bch.py
+++ src/peropq/bch.py
@@ -244,3 +244,64 @@ class VariationalNorm:
                     theta_coeff * left_term.coefficient * right_term.coefficient * trace
                 )
         return -s_norm
+
+    def get_analytical_gradient(self):
+        def get_i_derivative(theta_indices,theta_index):
+            position_list = []
+            for i,index_tuple in enumerate(theta_indices):
+                if theta_index==index_tuple:
+                    position_list.append(i)
+            indices_list_list= []
+            if len(position_list)>0:
+                for pos in position_list:
+                    indices_list = []
+                    for i,index_tuple in enumerate(theta_indices):
+                        if i!=pos:
+                            indices_list.append(index_tuple)
+                indices_list_list.append(indices_list)
+            return indices_list_list
+                
+        if self.unconstrained:
+            min_order = 0
+        else:
+            min_order = 1
+        s_norm: float = 0
+        self.gradient_theta_indices:list[list[list[int]]]=[]
+        self.gradient_vector_constants:list[list[float]]=[]
+        #########################################
+        # grad_{r,n} = sum_k sum_i [theta_{k,i,1}... theta_{k,i,n}] trace_k
+        # where i stands for the different derivatives of the index theta_{r,n}
+        # and k stands for the different terms of the norm
+        # self.gradient_vector_constants[counter,k]=trace_k
+        ########################################
+        counter = 0
+        for depth_index in range(self.variational_unitary.depth):
+            for term_index in range(self.variational_unitary.n_terms):
+                gradient_indices_list = []
+                trace_k_list = []
+                for i_trace, trace in enumerate(self.trace_list):
+                    left_term = self.all_the_terms[self.indices[i_trace][0]]
+                    right_term = self.all_the_terms[self.indices[i_trace][1]]
+                    if (left_term.order > min_order) and (right_term.order > min_order) and (None not in left_term.theta_indices) and (None not in right_term.theta_indices):
+                        indices_list_list = get_i_derivative(left_term.theta_indices+right_term.theta_indices,(depth_index,term_index))
+                        gradient_indices_list.append(indices_list_list)
+                        #TODO: add the trace correctly
+                        trace_k_list.append(trace)
+                counter+=1
+                self.gradient_theta_indices.append(gradient_indices_list)
+                self.gradient_vector_constants.append(trace_k_list)
+
+    def get_numerical_gradient(self,theta):
+        gradient = np.zeros(self.variational_unitary.depth*self.variational_unitary.n_terms)
+        for i,indices_list_list in enumerate(self.gradient_theta_indices):#coordinate
+            for k,indices_list in enumerate(self.indices_list_list):#trace
+                for l,derivative_indices in enumerate(self.indices_list):#sum of terms of the derivative
+                    product_theta = self.gradient_vector_constants[i][k]
+                    for index_tuple in derivative_indices:
+                        product_theta *= self.variational_unitary.theta[index_tuple]
+                    gradient[i] + = product_theta
+        return gradient
+
+            
+         
+
