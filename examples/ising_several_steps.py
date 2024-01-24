Gradient for constrained unitaries is now correct

examples/ising_several_steps.py
@@ -2,6 +2,7 @@ import copy
 
 import matplotlib.pyplot as plt
 import numpy as np
+from peropq.bch import VariationalNorm
 from peropq.ed_module import ExactDiagonalization as ED
 from peropq.hamiltonian import Hamiltonian
 from peropq.optimizer import Optimizer
@@ -22,8 +23,8 @@ z_list: list[PauliString] = []
 x_list: list[PauliString] = []
 y_list: list[PauliString] = []
 bc_modifier = 1
-nx = 2
-ny = 2
+nx = 3
+ny = 3
 n = nx * ny
 for i in range(n):
     zi = PauliString.from_pauli_sequence(paulis=[Pauli.Z], start_qubit=i)
@@ -33,8 +34,8 @@ for i in range(n):
     yi = PauliString.from_pauli_sequence(paulis=[Pauli.Y], start_qubit=i)
     y_list.append(yi)
 term_list = []
-for i in range(n):
-    term_list.append(1.0 * z_list[i])
+# for i in range(n):
+#     term_list.append(1.0 * z_list[i])
 for i in range(n):
     term_list.append(1.0 * x_list[i])
 V = -1
@@ -62,7 +63,7 @@ for site in start_sites:
 
 # Ising model
 h_ising = Hamiltonian(pauli_string_list=term_list)
-time_list = [0.3]
+time_list = [0.001]
 ed = ED(number_of_qubits=n)
 h_ising_matrix = ed.get_hamiltonian_matrix(hamiltonian=h_ising)
 
@@ -88,14 +89,47 @@ for time in time_list:
     variational_unitary.set_theta_to_trotter()
     # trotter_unitary = copy.deepcopy(variational_unitary)
     opt = Optimizer()
+    # test gradient
+    var_norm = VariationalNorm(
+        variational_unitary=variational_unitary, order=2, unconstrained=False
+    )
+    var_norm.get_commutators()
+    var_norm.get_traces()
+    norm_var = var_norm.calculate_norm(var_norm.variational_unitary.theta)
+    var_norm.get_analytical_gradient()
+    theta_flat = variational_unitary.flatten_theta(variational_unitary.theta)
+    grad = var_norm.get_numerical_gradient(theta_flat)
+    # approximate gradient
+    c2_theta = var_norm.calculate_norm(
+        variational_unitary.theta,
+    )
+    dt_grad = 0.0001
+    index_gradient = 6
+    theta_dt = copy.deepcopy(variational_unitary.theta)
+    theta_dt[0, index_gradient] = theta_dt[0, index_gradient] + dt_grad
+    variational_unitary.update_theta(theta_dt)
+    c2_theta_dt = var_norm.calculate_norm(variational_unitary.theta)
+    appr = (c2_theta_dt - c2_theta) / dt_grad
+    # analytical gradient
+    analytical = variational_unitary.c2_square_gradient_test(variational_unitary.theta)
+    print("c2_theta ", c2_theta)
+    print("c2_theta_dt", c2_theta_dt)
+    print("var_norm ", norm_var)
+    print("grad ", grad)
+    print("analytical ", analytical)
+    # print("appr ",grad[13]/appr)
+    print("appr ", appr)
+    ###############
     res = opt.optimize_arbitrary(
-        variational_unitary=variational_unitary, order=2, unconstrained=True
+        variational_unitary=variational_unitary,
+        order=2,
+        unconstrained=False,
     )
     print("res ", res)
     c2 = variational_unitary.c2_squared(variational_unitary.theta)
     print("c2 ", c2)
     c2_trotter = variational_unitary.c2_squared(
-        variational_unitary.get_initial_trotter_vector()
+        variational_unitary.get_initial_trotter_vector(),
     )
 # print("------")
 # print("constrained")
@@ -105,11 +139,10 @@ for time in time_list:
     trotter_unitary = copy.deepcopy(variational_unitary_c)
     opt = Optimizer()
     res = opt.optimize_arbitrary(
-        variational_unitary=variational_unitary_c,
-        order=3,
-        unconstrained=True,
-        initial_guess=variational_unitary.flatten_theta(variational_unitary.theta),
-    )
+        variational_unitary=variational_unitary_c, order=2, unconstrained=False
+    )  # ,
+    # initial_guess=variational_unitary.flatten_theta(variational_unitary_c.theta),
+    # )
     print("res ", res)
     # c2 = variational_unitary.c2_squared(variational_unitary.theta)
     # print("c2 ",c2)
@@ -123,10 +156,12 @@ state_continuous = copy.deepcopy(state_init)
 # Check that the norm is better for the variational unitary
 trotter_error = ed.get_error(trotter_unitary, hamiltonian=h_ising)
 variational_error = ed.get_error(
-    variational_unitary=variational_unitary, hamiltonian=h_ising
+    variational_unitary=variational_unitary,
+    hamiltonian=h_ising,
 )
 variational_c_error = ed.get_error(
-    variational_unitary=variational_unitary_c, hamiltonian=h_ising
+    variational_unitary=variational_unitary_c,
+    hamiltonian=h_ising,
 )
 print("trotter error", trotter_error)
 print("variational error order 2", variational_error)
@@ -135,22 +170,28 @@ print("variational error order 3", variational_c_error)
 for istep in range(10):
     state_trotter = ed.apply_variational_to_state(trotter_unitary, state_trotter)
     state_continuous = ed.apply_continuous_to_state(
-        hamiltonian=h_ising, time=time, state=state_continuous
+        hamiltonian=h_ising,
+        time=time,
+        state=state_continuous,
     )
     state_variational = ed.apply_variational_to_state(
-        variational_unitary, state_variational
+        variational_unitary,
+        state_variational,
     )
     state_variational_c = ed.apply_variational_to_state(
-        variational_unitary_c, state_variational_c
+        variational_unitary_c,
+        state_variational_c,
     )
 
     sz_expectation_value_trotter = expectation_value(state_trotter, z_list_sparse)
     sz_expectation_value_continuous = expectation_value(state_continuous, z_list_sparse)
     sz_expectation_value_variational = expectation_value(
-        state_variational, z_list_sparse
+        state_variational,
+        z_list_sparse,
     )
     sz_expectation_value_variational_c = expectation_value(
-        state_variational_c, z_list_sparse
+        state_variational_c,
+        z_list_sparse,
     )
 
     z_t_continous.append(sz_expectation_value_continuous)
@@ -160,10 +201,10 @@ for istep in range(10):
 
     energy_trotter.append(state_trotter.T.conj() @ h_ising_matrix @ state_trotter)
     energy_variational.append(
-        state_variational.T.conj() @ h_ising_matrix @ state_variational
+        state_variational.T.conj() @ h_ising_matrix @ state_variational,
     )
     energy_variational_c.append(
-        state_variational_c.T.conj() @ h_ising_matrix @ state_variational_c
+        state_variational_c.T.conj() @ h_ising_matrix @ state_variational_c,
     )
 
 z_array_continuous = np.array(z_t_continous)
@@ -211,7 +252,8 @@ plt.figure()
 plt.plot(np.abs(np.array(energy_trotter) - energy), label="trotter")
 plt.plot(np.abs(np.array(energy_variational) - energy), label="variational")
 plt.plot(
-    np.abs(np.array(energy_variational_c) - energy), label="variational constrained"
+    np.abs(np.array(energy_variational_c) - energy),
+    label="variational constrained",
 )
 plt.xlabel("time step")
 plt.ylabel("energy error")

src/peropq/bch.py
@@ -52,7 +52,10 @@ def commutator(aterm: NormTerm, other: NormTerm) -> NormTerm:
 
 class VariationalNorm:
     def __init__(
-        self, variational_unitary: VariationalUnitary, order: int, unconstrained=False
+        self,
+        variational_unitary: VariationalUnitary,
+        order: int,
+        unconstrained=False,
     ):
         self.variational_unitary = variational_unitary
         self.term_norm = []
@@ -63,7 +66,9 @@ class VariationalNorm:
         self.unconstrained = unconstrained
 
     def compute_commutator_sum(
-        self, term_list1: list[NormTerm], term_list2: list[NormTerm]
+        self,
+        term_list1: list[NormTerm],
+        term_list2: list[NormTerm],
     ) -> list[NormTerm]:
         result_list: list[NormTerm] = []
         for term1 in term_list1:
@@ -74,7 +79,6 @@ class VariationalNorm:
         return result_list
 
     def add_term(self, new_term: NormTerm):  # TODO: take care of coefficient
-        print("calling add term")
         # Third order
         if self.order >= 3:
             # print("existing terms ")
@@ -88,29 +92,25 @@ class VariationalNorm:
             for aterm in x_y_3:
                 aterm.coefficient = -0.5 * aterm.coefficient
             self.terms[2] += x_y_3
-            print("With order 2,from term")
             new_term.pretty_print()
-            print("x_y_3:")
             for aterm in x_y_3:
                 aterm.pretty_print()
             # Commutators first order:
             x_x_y_3 = self.compute_commutator_sum(
-                [new_term], self.compute_commutator_sum([new_term], self.terms[0])
+                [new_term],
+                self.compute_commutator_sum([new_term], self.terms[0]),
             )
             y_y_x_3 = []
             # for aterm_0 in self.terms[0]:
             #     y_y_x_3 += self.compute_commutator_sum([aterm_0],self.compute_commutator_sum([aterm_0],[new_term]))
             y_y_x_3 += self.compute_commutator_sum(
-                self.terms[0], self.compute_commutator_sum(self.terms[0], [new_term])
+                self.terms[0],
+                self.compute_commutator_sum(self.terms[0], [new_term]),
             )
-            print("x_x_y_3")
             for aterm in x_x_y_3:
                 aterm.pretty_print()
-            print("----------")
-            print("y_y_x_3")
             for aterm in y_y_x_3:
                 aterm.pretty_print()
-            print("----------")
             # breakpoint()
             for i_norm_term, norm_term in enumerate(x_x_y_3):
                 norm_term.coefficient = -(1.0 / 12.0) * norm_term.coefficient
@@ -246,28 +246,28 @@ class VariationalNorm:
         return -s_norm
 
     def get_analytical_gradient(self):
-        def get_i_derivative(theta_indices,theta_index):
+        def get_i_derivative(theta_indices, theta_index):
             position_list = []
-            for i,index_tuple in enumerate(theta_indices):
-                if theta_index==index_tuple:
+            for i, index_tuple in enumerate(theta_indices):
+                if theta_index == index_tuple:
                     position_list.append(i)
-            indices_list_list= []
-            if len(position_list)>0:
+            indices_list_list = []
+            if len(position_list) > 0:
                 for pos in position_list:
                     indices_list = []
-                    for i,index_tuple in enumerate(theta_indices):
-                        if i!=pos:
+                    for i, index_tuple in enumerate(theta_indices):
+                        if i != pos:
                             indices_list.append(index_tuple)
-                indices_list_list.append(indices_list)
+                    indices_list_list.append(indices_list)
             return indices_list_list
-                
+
         if self.unconstrained:
             min_order = 0
         else:
             min_order = 1
         s_norm: float = 0
-        self.gradient_theta_indices:list[list[list[int]]]=[]
-        self.gradient_vector_constants:list[list[float]]=[]
+        self.gradient_theta_indices: list[list[list[int]]] = []
+        self.gradient_vector_constants: list[list[float]] = []
         #########################################
         # grad_{r,n} = sum_k sum_i [theta_{k,i,1}... theta_{k,i,n}] trace_k
         # where i stands for the different derivatives of the index theta_{r,n}
@@ -282,26 +282,41 @@ class VariationalNorm:
                 for i_trace, trace in enumerate(self.trace_list):
                     left_term = self.all_the_terms[self.indices[i_trace][0]]
                     right_term = self.all_the_terms[self.indices[i_trace][1]]
-                    if (left_term.order > min_order) and (right_term.order > min_order) and (None not in left_term.theta_indices) and (None not in right_term.theta_indices):
-                        indices_list_list = get_i_derivative(left_term.theta_indices+right_term.theta_indices,(depth_index,term_index))
+                    if (
+                        (left_term.order > min_order)
+                        and (right_term.order > min_order)
+                        and ((None, None) not in left_term.theta_indices)
+                        and ((None, None) not in right_term.theta_indices)
+                    ):
+                        indices_list_list = get_i_derivative(
+                            left_term.theta_indices + right_term.theta_indices,
+                            (depth_index, term_index),
+                        )
                         gradient_indices_list.append(indices_list_list)
-                        #TODO: add the trace correctly
-                        trace_k_list.append(trace)
-                counter+=1
+                        trace_k_list.append(
+                            trace * left_term.coefficient * right_term.coefficient
+                        )
+                counter += 1
                 self.gradient_theta_indices.append(gradient_indices_list)
                 self.gradient_vector_constants.append(trace_k_list)
 
-    def get_numerical_gradient(self,theta):
-        gradient = np.zeros(self.variational_unitary.depth*self.variational_unitary.n_terms)
-        for i,indices_list_list in enumerate(self.gradient_theta_indices):#coordinate
-            for k,indices_list in enumerate(self.indices_list_list):#trace
-                for l,derivative_indices in enumerate(self.indices_list):#sum of terms of the derivative
+    def get_numerical_gradient(self, theta_flat):
+        theta = theta_flat.reshape(
+            (self.variational_unitary.depth, self.variational_unitary.n_terms)
+        )
+        self.variational_unitary.theta = theta
+        gradient = np.zeros(
+            self.variational_unitary.depth * self.variational_unitary.n_terms
+        )
+        for i, indices_list_list in enumerate(
+            self.gradient_theta_indices
+        ):  # coordinate
+            for k, indices_list in enumerate(indices_list_list):  # trace
+                for l, derivative_indices in enumerate(
+                    indices_list
+                ):  # sum of terms of the derivative
                     product_theta = self.gradient_vector_constants[i][k]
                     for index_tuple in derivative_indices:
                         product_theta *= self.variational_unitary.theta[index_tuple]
-                    gradient[i] + = product_theta
-        return gradient
-
-            
-         
-
+                    gradient[i] += product_theta
+        return -gradient

src/peropq/optimizer.py
@@ -71,17 +71,17 @@ class Optimizer:
             pass
         variational_norm.get_traces()
         if tol == 0:
+            variational_norm.get_analytical_gradient()
             optimized_results = scipy.optimize.minimize(
                 variational_norm.calculate_norm,
                 x0,
-                method="Nelder-Mead",
+                jac=variational_norm.get_numerical_gradient,
             )
         else:
             optimized_results = scipy.optimize.minimize(
                 variational_norm.calculate_norm,
                 x0,
                 tol=tol,
-                method="Nelder-Mead",
             )
         return optimized_results
 

src/peropq/variational_unitary.py
@@ -197,7 +197,7 @@ class VariationalUnitary:
         for layer in range(self.depth):
             for iterm in range(self.n_terms):
                 full_term_list.append(
-                    -1j * self.theta[layer, iterm] * self.pauli_string_list[iterm]
+                    -1j * self.theta[layer, iterm] * self.pauli_string_list[iterm],
                 )
 
         number_of_terms = len(full_term_list)
@@ -206,16 +206,107 @@ class VariationalUnitary:
             for i in range(j, number_of_terms):
                 commutator_list.append(
                     0.5
-                    * get_commutator_pauli_tensors(full_term_list[i], full_term_list[j])
+                    * get_commutator_pauli_tensors(
+                        full_term_list[i], full_term_list[j]
+                    ),
                 )
         trace_sum = 0
+        trace_list = []
         for commutator_i in commutator_list:
             for commutator_j in commutator_list:
                 product_commutator: PauliString = commutator_i * commutator_j
                 # print("product commutator ",product_commutator)
                 if product_commutator != 0.0:
+                    trace_list.append(normalized_trace())
                     trace_sum += product_commutator.normalized_trace()
+        print(trace_list)
+        breakpoint()
         return -trace_sum
+
+    def c2_square_gradient_test(self, theta: npt.ArrayLike = ()) -> npt.ArrayLike:
+        self.update_theta(theta)
+        full_term_list = []
+        tuple_list = []
+        for layer in range(self.depth):
+            for iterm in range(self.n_terms):
+                full_term_list.append(
+                    -1j * self.pauli_string_list[iterm],
+                )
+                tuple_list.append((layer, iterm))
+
+        number_of_terms = len(full_term_list)
+        commutator_list = []
+        indices_list = []
+        for j in range(number_of_terms):
+            for i in range(j, number_of_terms):
+                commutator_list.append(
+                    0.5
+                    * get_commutator_pauli_tensors(
+                        full_term_list[i], full_term_list[j]
+                    ),
+                )
+                indices_list.append((i, j))
+        grad = np.zeros((number_of_terms,))
+        for i_derivative in range(number_of_terms):
+            for i, commutator_i in enumerate(commutator_list):
+                for j, commutator_j in enumerate(commutator_list):
+                    full_theta_coeff = (
+                        self.theta[tuple_list[indices_list[i][0]]]
+                        * self.theta[tuple_list[indices_list[i][1]]]
+                        * self.theta[tuple_list[indices_list[j][0]]]
+                        * self.theta[tuple_list[indices_list[j][1]]]
+                    )
+                    product_commutator = commutator_i * commutator_j
+                    theta_coeff = 0.0
+                    print("i_derivative ", i_derivative)
+                    print(tuple_list[indices_list[i][0]])
+                    print(tuple_list[indices_list[i][1]])
+                    print(tuple_list[indices_list[j][0]])
+                    print(tuple_list[indices_list[j][1]])
+                    try:
+                        print("product_commutator.normalized_trace()")
+                        print(product_commutator.normalized_trace())
+                        # breakpoint()
+                    except:
+                        pass
+                    if i_derivative == indices_list[i][0]:
+                        coeff = (
+                            full_theta_coeff
+                            / self.theta[tuple_list[indices_list[i][0]]]
+                        )
+                        if product_commutator != 0.0:
+                            grad[i_derivative] -= (
+                                product_commutator.normalized_trace() * coeff
+                            )
+                    if i_derivative == indices_list[i][1]:
+                        coeff = (
+                            full_theta_coeff
+                            / self.theta[tuple_list[indices_list[i][1]]]
+                        )
+                        if product_commutator != 0.0:
+                            grad[i_derivative] -= (
+                                product_commutator.normalized_trace() * coeff
+                            )
+                    if i_derivative == indices_list[j][0]:
+                        coeff = (
+                            full_theta_coeff
+                            / self.theta[tuple_list[indices_list[j][0]]]
+                        )
+                        if product_commutator != 0.0:
+                            grad[i_derivative] -= (
+                                product_commutator.normalized_trace() * coeff
+                            )
+                    if i_derivative == indices_list[j][1]:
+                        coeff = (
+                            full_theta_coeff
+                            / self.theta[tuple_list[indices_list[j][1]]]
+                        )
+                        if product_commutator != 0.0:
+                            grad[i_derivative] -= (
+                                product_commutator.normalized_trace() * coeff
+                            )
+                    # print('grad ',grad[i_derivative])
+        return grad
 
     def c3_squared_test(self, theta: npt.ArrayLike = ()) -> float:
         self.update_theta(theta)
@@ -224,7 +315,7 @@ class VariationalUnitary:
         for layer in range(self.depth):
             for iterm in range(self.n_terms):
                 full_term_list.append(
-                    -1j * self.theta[layer, iterm] * self.pauli_string_list[iterm]
+                    -1j * self.theta[layer, iterm] * self.pauli_string_list[iterm],
                 )
                 theta_index_list.append((layer, iterm))
         number_of_terms = len(full_term_list)
@@ -235,7 +326,9 @@ class VariationalUnitary:
             for i in range(j, number_of_terms):
                 commutator_list.append(
                     0.5
-                    * get_commutator_pauli_tensors(full_term_list[i], full_term_list[j])
+                    * get_commutator_pauli_tensors(
+                        full_term_list[i], full_term_list[j]
+                    ),
                 )
                 indices_list.append((theta_index_list[i], theta_index_list[j]))
         # Third order
@@ -244,7 +337,8 @@ class VariationalUnitary:
             for k in range(number_of_terms):
                 if j != k:
                     com = get_commutator_pauli_tensors(
-                        full_term_list[j], full_term_list[k]
+                        full_term_list[j],
+                        full_term_list[k],
                     )
                     if com != 0.0:
                         com_com = get_commutator_pauli_tensors(full_term_list[j], com)
@@ -254,18 +348,20 @@ class VariationalUnitary:
                                 theta_index_list[j],
                                 theta_index_list[j],
                                 theta_index_list[k],
-                            )
+                            ),
                         )
         # Second term
         for l in range(number_of_terms):
             for k in range(l + 1, number_of_terms):
                 for j in range(k + 1, number_of_terms):
                     com_kl = get_commutator_pauli_tensors(
-                        full_term_list[k], full_term_list[l]
+                        full_term_list[k],
+                        full_term_list[l],
                     )
                     if com_kl != 0.0:
                         com_jkl = get_commutator_pauli_tensors(
-                            full_term_list[j], com_kl
+                            full_term_list[j],
+                            com_kl,
                         )
                         commutator_list.append(1.0 / 6.0 * com_jkl)
                         indices_list.append(
@@ -273,14 +369,16 @@ class VariationalUnitary:
                                 theta_index_list[j],
                                 theta_index_list[k],
                                 theta_index_list[l],
-                            )
+                            ),
                         )
                     com_kj = get_commutator_pauli_tensors(
-                        full_term_list[k], full_term_list[j]
+                        full_term_list[k],
+                        full_term_list[j],
                     )
                     if com_kj != 0.0:
                         com_lkj = get_commutator_pauli_tensors(
-                            full_term_list[l], com_kj
+                            full_term_list[l],
+                            com_kj,
                         )
                         commutator_list.append(1.0 / 6.0 * com_lkj)
                         indices_list.append(
@@ -288,7 +386,7 @@ class VariationalUnitary:
                                 theta_index_list[l],
                                 theta_index_list[k],
                                 theta_index_list[j],
-                            )
+                            ),
                         )
 
         # Calculate traces: