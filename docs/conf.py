c5e1e229f39844e0c7c01c54b479abee0750b411
Travis Thompson <102229498+trvto@users.noreply.github.com>
2024-02-07 16:43:01 +0100

    Fix Read the Docs (#26)
    * add requirements section
    
    * update docs/conf.py
    
    * try autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,10 @@ author = "Quantinuum"
 
 extensions = [
     "sphinx.ext.autodoc",
+    "autoapi.extension",
     "sphinx.ext.napoleon",
     "myst_parser",
 ]
+autoapi_dirs = ["../src"]
 autodoc_typehints = "description"
 html_theme = "furo"
