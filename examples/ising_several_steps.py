Fixed the sign for the third order in BCH

examples/ising_several_steps.py
@@ -23,8 +23,8 @@ z_list: list[PauliString] = []
 x_list: list[PauliString] = []
 y_list: list[PauliString] = []
 bc_modifier = 1
-nx = 3
-ny = 3
+nx = 2
+ny = 2
 n = nx * ny
 np.random.seed(0)
 for i in range(n):
@@ -36,11 +36,11 @@ for i in range(n):
     y_list.append(yi)
 term_list = []
 for i in range(n):
-    # term_list.append( z_list[i])
-    term_list.append(np.random.rand() * z_list[i])
+    term_list.append( z_list[i])
+    # term_list.append(np.random.rand() * z_list[i])
 for i in range(n):
-    # term_list.append( x_list[i])
-    term_list.append(np.random.rand() * x_list[i])
+    term_list.append( x_list[i])
+    # term_list.append(np.random.rand() * x_list[i])
 V = -1
 # vertical bonds
 for col in range(nx):
@@ -50,8 +50,8 @@ for col in range(nx):
     print("vertical bonds")
     for isite in range(len(v_list) - bc_modifier):
         term_list.append(
-            np.random.rand()*z_list[v_list[isite]] * z_list[v_list[(isite + 1) % len(v_list)]],
-            # z_list[v_list[isite]] * z_list[v_list[(isite + 1) % len(v_list)]],
+            # np.random.rand()*z_list[v_list[isite]] * z_list[v_list[(isite + 1) % len(v_list)]],
+            z_list[v_list[isite]] * z_list[v_list[(isite + 1) % len(v_list)]],
         )
         print(v_list[isite], v_list[(isite + 1) % len(v_list)])
 
@@ -90,7 +90,7 @@ state_init[random_integer] = 1.0
 energy = state_init.T @ h_ising_matrix @ state_init
 # print("unconstrained ")
 # print("-------")
-nlayer = 3
+nlayer = 1
 variational_unitary = VU(h_ising, number_of_layer=nlayer, time=time_list[0])
 variational_unitary.set_theta_to_trotter()
 for itime,time in enumerate(time_list):
@@ -134,7 +134,7 @@ for itime,time in enumerate(time_list):
     theta_flat = variational_unitary.flatten_theta(variational_unitary.theta)
     res = opt.optimize_arbitrary(
         variational_unitary=variational_unitary,
-        order=3,
+        order=2,
         unconstrained=True,
         initial_guess=theta_flat,
     )
@@ -154,7 +154,7 @@ for time in time_list:
     theta_flat = variational_unitary_c.flatten_theta(variational_unitary_c.theta)
     res = opt.optimize_arbitrary(
         variational_unitary=variational_unitary_c,
-        order=4,
+        order=3,
         unconstrained=True,
         initial_guess=theta_flat
     )  # ,

examples/random_ising.py
@@ -26,8 +26,8 @@ z_list: list[PauliString] = []
 x_list: list[PauliString] = []
 y_list: list[PauliString] = []
 bc_modifier = 1
-nx = 3
-ny = 3
+nx = 2
+ny = 2
 n = nx*ny
 for i in range(n):
     zi = PauliString.from_pauli_sequence(paulis=[Pauli.Z], start_qubit=i)
@@ -68,13 +68,14 @@ h_ising = Hamiltonian(pauli_string_list=term_list)
 # time_list=[0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.3,1.6,1.9]
 # time_list = [0.001,0.005,0.01,0.02,0.03,0.04,0.05]
 time_list = [0.3]
+nlayer = 1
 ed =  ED(number_of_qubits=n)
 h_ising_matrix = ed.get_hamiltonian_matrix(hamiltonian=h_ising)
 if norm_mode:
     trotter_error_list = []
     variational_error_list = []
     for time in time_list:
-        variational_unitary = VariationalUnitary(h_ising, number_of_layer=3, time=time)
+        variational_unitary = VariationalUnitary(h_ising, number_of_layer=nlayer, time=time)
         variational_unitary.set_theta_to_trotter()
         trotter_error = ed.get_error(variational_unitary=variational_unitary,hamiltonian=h_ising)
         opt = Optimizer()
@@ -118,7 +119,7 @@ if observable_mode:
     state_init = np.array([1.0+0.0j]+[0.0]*(2**n-1))
     energy = state_init.T@h_ising_matrix@state_init
     for time in time_list:
-        variational_unitary = VariationalUnitary(h_ising, number_of_layer=3, time=time)
+        variational_unitary = VariationalUnitary(h_ising, number_of_layer=nlayer, time=time)
         variational_unitary.set_theta_to_trotter()
         state_trotter = ed.apply_variational_to_state(variational_unitary,state_init)
         state_continuous = ed.apply_continuous_to_state(hamiltonian=h_ising,time=time,state = state_init)

examples/test_exact_norm.py
@@ -25,14 +25,14 @@ def expectation_value(state, obs_list):
 # Random?
 random_hamiltonian= True
 # Tilted or transverse?
-hamiltonian_type = 'transverse'
+hamiltonian_type = 'tilted'
 
 z_list: list[PauliString] = []
 x_list: list[PauliString] = []
 y_list: list[PauliString] = []
 bc_modifier = 1
-nx = 2
-ny = 2
+nx = 3
+ny = 3
 n = nx * ny
 np.random.seed(0)
 for i in range(n):
@@ -91,7 +91,7 @@ for site in start_sites:
 
 # Ising model
 h_ising = Hamiltonian(pauli_string_list=term_list)
-time_list = [0.1]
+time_list = [0.3]
 ed = ED(number_of_qubits=n)
 h_ising_matrix = ed.get_hamiltonian_matrix(hamiltonian=h_ising)
 
@@ -114,6 +114,22 @@ random_integer = np.random.randint(low=0,high=2**n)
 state_init[random_integer] = 1.0
 energy = state_init.T @ h_ising_matrix @ state_init
 nlayer = 3
+
+variational_unitary_c = VU(h_ising, number_of_layer=nlayer, time=time_list[0])
+variational_unitary_c.set_theta_to_trotter()
+trotter_unitary = copy.deepcopy(variational_unitary_c)
+for time in time_list:
+    variational_unitary_c.time = time
+    opt = Optimizer()
+    theta_flat = variational_unitary_c.flatten_theta(variational_unitary_c.theta)
+    res = opt.optimize_arbitrary(
+        variational_unitary=variational_unitary_c,
+        order=2,
+        unconstrained=True,
+    )  
+    theta_flat = variational_unitary_c.flatten_theta(variational_unitary_c.theta)
+    print("order 2 res ", res)
+
 variational_3_unitary = VU(h_ising, number_of_layer=nlayer, time=time_list[0])
 variational_3_unitary.set_theta_to_trotter()
 for itime,time in enumerate(time_list):
@@ -128,22 +144,8 @@ for itime,time in enumerate(time_list):
         unconstrained=True,
         initial_guess=theta_flat,
     )
+    theta_flat = variational_3_unitary.flatten_theta(variational_3_unitary.theta)
     print("order 3 res ", res)
-
-variational_unitary_c = VU(h_ising, number_of_layer=nlayer, time=time_list[0])
-variational_unitary_c.set_theta_to_trotter()
-trotter_unitary = copy.deepcopy(variational_unitary_c)
-for time in time_list:
-    variational_unitary_c.time = time
-    opt = Optimizer()
-    theta_flat = variational_unitary_c.flatten_theta(variational_unitary_c.theta)
-    res = opt.optimize_arbitrary(
-        variational_unitary=variational_unitary_c,
-        order=2,
-        unconstrained=True,
-    )  
-    print("order 2 res ", res)
-
 ########
 # Do the optimization with ExactUnitary
 

examples/test_single_qubit.py
@@ -21,29 +21,98 @@ def expectation_value(state, obs_list):
         expectation_value_list.append(state.T.conj() @ obs @ state)
     return expectation_value_list
 
+def get_bch_norm(variational_unitary,order):
+    variational_norm = VariationalNorm(
+        variational_unitary=variational_unitary, order=order, unconstrained=True
+    )
+    variational_norm.get_commutators()
+    variational_norm.get_traces()
+    c3 = variational_norm.calculate_norm(variational_unitary.theta)
+    return c3
+    
 
 #Hardcode the analytical function
-def analytics(order,time):
-    def analytic_perturbative_norm_2(theta):
+class Analytics:
+    def __init__(self,order,time):
+        self.order = order
+        self.time = time    
+    def analytic_perturbative_norm_2(self,theta):
         x = -1j*theta[0]
         y = -1j*theta[1]
-        first_order = (-1j*time-x)*(+1j*time+x) + (-1j*time-y)*(+1j*time+y)
+        first_order = (-1j*self.time-x)*(+1j*self.time+x) + (-1j*self.time-y)*(+1j*self.time+y)
         second_order = x**2*y**2
         return np.real(first_order+second_order)
-    def analytic_perturbative_norm_3(theta):
+    def analytic_perturbative_norm_3(self,theta):
         x = -1j*theta[0]
         y = -1j*theta[1]
-        second_order = analytic_perturbative_norm_2(theta)
-        third_order = -(4/12)**2*x**4*y**2-(4/12)**2*x**2*y**4
+        second_order = self.analytic_perturbative_norm_2(theta)
+        third_order = -(4.0/12.0)**2*x**4*y**2-(4.0/12.0)**2*x**2*y**4
+        third_order += (8.0/12.0)*(-1j*self.time-x)*x*y**2
+        third_order += (8.0/12.0)*(-1j*self.time-y)*x**2*y
+        print("third order",third_order)
         return second_order+np.real(third_order)
-    def optimize(order,theta_init):
-        if order==2:
-            optimized_results = scipy.optimize.minimize(analytic_perturbative_norm_2, theta_init)
-        if order ==3:
-            optimized_results = scipy.optimize.minimize(analytic_perturbative_norm_3, theta_init)
+    def optimize(self,theta_init):
+        if self.order==2:
+            optimized_results = scipy.optimize.minimize(self.analytic_perturbative_norm_2, theta_init)
+        if self.order ==3:
+            optimized_results = scipy.optimize.minimize(self.analytic_perturbative_norm_3, theta_init)
         return optimized_results
-    return optimize(order,[time_list[0],time_list[0]])
-        
+
+class Analitics_x_y_x:
+                
+    def __init__(self,order,time):
+        self.order = order
+        self.time = time    
+
+    def trace_list_list(self,alist):
+        def trace_list(alist):
+            s=0
+            for element_a in alist:
+                for element_b in alist:
+                    s+=element_a*np.conjugate(element_b)
+            return s
+        s= 0
+        for the_list in alist:
+            s+=trace_list(the_list)
+        return s
+
+    def analytic_perturbative_norm_2(self,theta):
+        x = -1j*theta[0]
+        y = -1j*theta[1]
+        x2 = -1j*theta[2]
+        term_list_x = []
+        term_list_x.append((-1j*self.time-x))
+        term_list_x.append((-1j*self.time-x2))
+        term_list_y = []
+        term_list_y.append(-1j*self.time-y)
+        term_list_z = [+1j*y*x2-1j*x*y]
+        return self.trace_list_list([term_list_x,term_list_y,term_list_z])
+
+    def analytic_perturbative_norm_3(self,theta):
+        x = -1j*theta[0]
+        y = -1j*theta[1]
+        x2 = -1j*theta[2]
+        term_list_x = []
+        term_list_x.append((-1j*self.time-x))
+        term_list_x.append((-1j*self.time-x2))
+        term_list_x.append(-(4.0/12.0)*y**2*x2)
+        term_list_y = []
+        term_list_y.append(-1j*self.time-y)
+        term_list_y.append(+x*y*x2)
+        term_list_y.append(+(4.0/12.0)*x*y*x2)
+        term_list_y.append(-(4.0/12.0)*y*x2**2)
+        term_list_y.append(-(4.0/12.0)*x**2*y)
+        term_list_z = [+1j*y*x2-1j*x*y]
+        return self.trace_list_list([term_list_x,term_list_y,term_list_z])
+
+    def optimize(self,theta_init):
+        if self.order==2:
+            optimized_results = scipy.optimize.minimize(self.analytic_perturbative_norm_2, theta_init)
+        if self.order ==3:
+            optimized_results = scipy.optimize.minimize(self.analytic_perturbative_norm_3, theta_init)
+        return optimized_results
+
+            
 #############
 # Options 
 # Random?
@@ -69,9 +138,10 @@ for i in range(n):
 term_list = []
 term_list.append(x_list[0])
 term_list.append(y_list[0])
+term_list.append(x_list[0])
 # Ising model
 h_ising = Hamiltonian(pauli_string_list=term_list)
-time_list = [0.3]
+time_list = [0.4]
 ed = ED(number_of_qubits=n)
 h_ising_matrix = ed.get_hamiltonian_matrix(hamiltonian=h_ising)
 
@@ -96,6 +166,10 @@ energy = state_init.T @ h_ising_matrix @ state_init
 nlayer = 1
 variational_3_unitary = VU(h_ising, number_of_layer=nlayer, time=time_list[0])
 variational_3_unitary.set_theta_to_trotter()
+###################################
+#Analytics order 3
+analytics = Analitics_x_y_x(order=3,time=time_list[0])
+res_analytics = analytics.optimize([time_list[0],time_list[0],time_list[0]])
 for itime,time in enumerate(time_list):
     variational_3_unitary.time=time
     trotter_unitary = copy.deepcopy(variational_3_unitary)
@@ -110,8 +184,24 @@ for itime,time in enumerate(time_list):
     )
     print("order 3 res ", res)
 
-res = analytics(order=3,time=time_list[0])
-print("res analytics 3 ",res)
+####################################
+# See if the norm is the same for the analytics and implementation
+# theta_flat = variational_3_unitary.flatten_theta(variational_3_unitary.theta)
+# norm_implementation = get_bch_norm(variational_3_unitary,order=3)
+# second_order_implementation = get_bch_norm(variational_3_unitary,order=2)
+# print("third order implementation ",norm_implementation-second_order_implementation)
+# norm_analytics = analytics.analytic_perturbative_norm_3(theta_flat)
+# norm_test = variational_3_unitary.c2_squared_test(variational_3_unitary.theta)
+# print("implementation ",norm_implementation)
+# print("analytics ",norm_analytics)
+# print("test ",norm_test)
+###################################
+
+print("res analytics 3 ",res_analytics)
+x_analytics = res_analytics.x
+# analytics_unitary = VU(h_ising, number_of_layer=nlayer, time=time_list[0])
+# analytics_unitary.theta = np.array(x_analytics).reshape((1,h_ising.get_n_terms()))
+# variational_3_unitary = analytics_unitary
 
 variational_unitary_c = VU(h_ising, number_of_layer=nlayer, time=time_list[0])
 variational_unitary_c.set_theta_to_trotter()
@@ -129,7 +219,8 @@ for time in time_list:
 
 ########
 # Do the optimization with ExactUnitary
-res = analytics(order=2,time=time_list[0])
+analytics = Analytics(order=2,time=time_list[0])
+res = analytics.optimize([time_list[0],time_list[0]])
 print("res analytics ",res)
 exact_unitary = ExactUnitary(h_ising, number_of_layer=nlayer, time=time_list[0],number_of_qubits=n)
 exact_unitary.set_theta_to_trotter()

src/peropq/bch.py
@@ -5,6 +5,7 @@ import numpy as np
 from peropq import commutators
 from peropq.pauli import PauliString
 from peropq.variational_unitary import VariationalUnitary
+import copy
 
 """
 BCH formula given by:
@@ -23,7 +24,7 @@ class NormTerm:
 
     pauli_string: PauliString
     order: int
-    coefficient: float
+    coefficient: complex
     theta_indices: list[int]
 
     def pretty_print(self):
@@ -34,8 +35,6 @@ class NormTerm:
             pauli_print_string += str(self.pauli_string.qubit_pauli_map[akey])
             pauli_print_string += str(akey) + " "
         print(self.coefficient, "*", pauli_print_string, self.theta_indices)
-        # print("theta indices ",self.theta_indices)
-        # print("***")
 
 
 def commutator(aterm: NormTerm, other: NormTerm) -> NormTerm:
@@ -84,7 +83,7 @@ class VariationalNorm:
             #2nd order with terms of order 3
             x_y_4 = self.compute_commutator_sum([new_term], self.terms[2])
             for aterm in x_y_4:
-                aterm.coefficient = -0.5 * aterm.coefficient
+                aterm.coefficient = +0.5 * aterm.coefficient
             self.terms[3] += x_y_4
             # 3rd order with terms of order 2
             x_x_y_4 = self.compute_commutator_sum(
@@ -113,7 +112,7 @@ class VariationalNorm:
             # Commutators with terms of order 2:
             x_y_3 = self.compute_commutator_sum([new_term], self.terms[1])
             for aterm in x_y_3:
-                aterm.coefficient = -0.5 * aterm.coefficient
+                aterm.coefficient = +0.5 * aterm.coefficient
             self.terms[2] += x_y_3
             x_x_y_3 = self.compute_commutator_sum(
                 [new_term],
@@ -125,9 +124,9 @@ class VariationalNorm:
                 self.compute_commutator_sum(self.terms[0], [new_term]),
             )
             for i_norm_term, norm_term in enumerate(x_x_y_3):
-                norm_term.coefficient = -(1.0 / 12.0) * norm_term.coefficient
+                norm_term.coefficient = +(1.0 / 12.0) * norm_term.coefficient
             for i_norm_term, norm_term in enumerate(y_y_x_3):
-                norm_term.coefficient = -(1.0 / 12.0) * norm_term.coefficient
+                norm_term.coefficient = +(1.0 / 12.0) * norm_term.coefficient
             self.terms[2] += x_x_y_3
             self.terms[2] += y_y_x_3
         # Second order:
@@ -189,8 +188,14 @@ class VariationalNorm:
                     theta_indices=[(None, None)],
                 )
                 self.terms[0].append(new_term)
+        # for aterm in self.terms[2]:
+        #     aterm.pretty_print()
 
     def get_traces(self):
+        def string_string_dagger(string1,string2):
+            string_dagger:PauliString = copy.deepcopy(string2)
+            string_dagger.coefficient = np.conjugate(string_dagger.coefficient)
+            return string1*string_dagger
         self.indices: list[tuple] = []
         self.trace_list: list[float] = []
         self.all_the_terms: list[NormTerm] = []
@@ -201,10 +206,14 @@ class VariationalNorm:
         for i_term, a_term in enumerate(self.all_the_terms):
             for j_term, another_term in enumerate(self.all_the_terms):
                 product_commutators: PauliString = (
-                    a_term.pauli_string * another_term.pauli_string
+                    # a_term.pauli_string * another_term.pauli_string
+                    string_string_dagger(a_term.pauli_string,another_term.pauli_string)
                 )
                 trace: complex = product_commutators.normalized_trace()
                 if trace:
+                    # print("a_term ",a_term)
+                    # print("another_term ",another_term)
+                    # print("trace ",trace)
                     self.indices.append((i_term, j_term))
                     self.trace_list.append(trace)
         self.calculated_trace = True
@@ -251,10 +260,16 @@ class VariationalNorm:
                 for i_theta in right_term.theta_indices:
                     if None not in i_theta:
                         theta_coeff *= self.variational_unitary.theta[i_theta]
+                # print("left right contribution")
+                # print(left_term)
+                # print(right_term)
+                # print(left_term.coefficient * right_term.coefficient * trace)
+                # breakpoint()
                 s_norm += (
-                    theta_coeff * left_term.coefficient * right_term.coefficient * trace
+                    # theta_coeff * left_term.coefficient * right_term.coefficient * trace
+                    theta_coeff * left_term.coefficient * np.conjugate(right_term.coefficient) * trace
                 )
-        return -s_norm
+        return +s_norm
 
     def get_analytical_gradient(self):
         def get_i_derivative(theta_indices, theta_index):

src/peropq/optimizer.py
@@ -74,14 +74,14 @@ class Optimizer:
             optimized_results = scipy.optimize.minimize(
                 variational_norm.calculate_norm,
                 x0,
-                jac=variational_norm.get_numerical_gradient,
+                # jac=variational_norm.get_numerical_gradient,
             )
         else:
             optimized_results = scipy.optimize.minimize(
                 variational_norm.calculate_norm,
                 x0,
                 tol=tol,
-                jac=variational_norm.get_numerical_gradient,
+                # jac=variational_norm.get_numerical_gradient,
             )
         return optimized_results
 

src/peropq/variational_unitary.py
@@ -191,10 +191,20 @@ class VariationalUnitary:
         return np.real(-chi_tensor.T @ trace_tensor @ chi_tensor)
 
     def c2_squared_test(self, theta: npt.ArrayLike = ()) -> float:
+        def product_dagger(string1,string2):
+            if string1!=0 and string2!=0:
+                import copy
+                string_dag = copy.deepcopy(string2)
+                string_dag.coefficient = np.conjugate(string_dag.coefficient)
+                return string1*string_dag
+            else:
+                return 0
         self.update_theta(theta)
         full_term_list = []
+        full_term_list_without_theta = []
         for layer in range(self.depth):
             for iterm in range(self.n_terms):
+                full_term_list_without_theta.append(self.cjs[iterm]*self.pauli_string_list[iterm])
                 full_term_list.append(
                     -1j * self.theta[layer, iterm] * self.pauli_string_list[iterm],
                 )
@@ -212,13 +222,17 @@ class VariationalUnitary:
                 )
         trace_sum = 0
         trace_list = []
+        # First order
+        # for j in range(number_of_terms):
+        #     commutator_list.append(1j*full_term_list_without_theta[j])
+        #     indices_list.append((theta_index_list[j]))
         for commutator_i in commutator_list:
             for commutator_j in commutator_list:
-                product_commutator: PauliString = commutator_i * commutator_j
-                # print("product commutator ",product_commutator)
+                # product_commutator: PauliString = commutator_i * commutator_j
+                product_commutator: PauliString = product_dagger(commutator_i,commutator_j)
                 if product_commutator != 0.0:
                     trace_sum += product_commutator.normalized_trace()
-        return -trace_sum
+        return trace_sum
 
     def c2_square_gradient_test(self, theta: npt.ArrayLike = ()) -> npt.ArrayLike:
         self.update_theta(theta)
@@ -296,19 +310,33 @@ class VariationalUnitary:
         return grad
 
     def c3_squared_test(self, theta: npt.ArrayLike = ()) -> float:
+        def product_dagger(string1,string2):
+            if string1!=0 and string2!=0:
+                import copy
+                string_dag = copy.deepcopy(string2)
+                string_dag.coefficient = np.conjugate(string_dag.coefficient)
+                return string1*string_dag
+            else:
+                return 0
         self.update_theta(theta)
         full_term_list = []
+        full_term_list_without_theta = []
         theta_index_list = []
         for layer in range(self.depth):
             for iterm in range(self.n_terms):
                 full_term_list.append(
                     -1j * self.theta[layer, iterm] * self.pauli_string_list[iterm],
                 )
+                full_term_list_without_theta.append(self.cjs[iterm]*self.pauli_string_list[iterm])
                 theta_index_list.append((layer, iterm))
         number_of_terms = len(full_term_list)
-        # Second order
         commutator_list = []
         indices_list = []
+        # First order
+        # for j in range(number_of_terms):
+        #     commutator_list.append(1j*full_term_list_without_theta[j])
+        #     indices_list.append((theta_index_list[j]))
+        # Second order
         for j in range(number_of_terms):
             for i in range(j, number_of_terms):
                 commutator_list.append(
@@ -382,7 +410,9 @@ class VariationalUnitary:
         for i, commutator_i in enumerate(commutator_list):
             for commutator_j in commutator_list:
                 product_commutator: PauliString = commutator_i * commutator_j
+                # product_commutator: PauliString = product_dagger(commutator_i,commutator_j)
                 # print("product commutator ",product_commutator)
                 if product_commutator != 0.0:
                     trace_sum -= product_commutator.normalized_trace()
+
         return trace_sum